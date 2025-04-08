d55ae716a737d276c2d3853f2253830bfd1c163a
bmeistergeneral <brecon.morgan@gmail.com>
2025-04-08 12:01:15 -0400

    Added comments

--- 01--print-horizontal-histogram-of-length-of-words-using-function-pointers.c
+++ 01--print-horizontal-histogram-of-length-of-words-using-function-pointers.c
@@ -2,16 +2,18 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+// Function to swap two integer elements in an array
 void swap(int* element1Ptr, int* element2Ptr) {
     int temp = *element1Ptr;
     *element1Ptr = *element2Ptr;
     *element2Ptr = temp;
 }
-
+// Comparison function for ascending order
 bool ascending(int a, int b) {
     return a > b;
 }
 
+// Comparison function for descending order
 bool descending(int a, int b) {
     return a < b;
 }
