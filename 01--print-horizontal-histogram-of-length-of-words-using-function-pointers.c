b7649220f23f9c5372efa125313107ae0ea9bcc5
bmeistergeneral <brecon.morgan@gmail.com>
2025-04-08 11:34:33 -0400

    Changed first program
    Added swap(), ascending(), and descending() functions for bubble sort algorithm

--- 01--print-horizontal-histogram-of-length-of-words-using-function-pointers.c
+++ 01--print-horizontal-histogram-of-length-of-words-using-function-pointers.c
@@ -1,1 +1,18 @@
-int main(){}
+#include <stdio.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+void swap(int* element1Ptr, int* element2Ptr) {
+    int temp = *element1Ptr;
+    *element1Ptr = *element2Ptr;
+    *element2Ptr = temp;
+}
+
+bool ascending(int a, int b) {
+    return a > b;
+}
+
+bool descending(int a, int b) {
+    return a < b;
+}
+
